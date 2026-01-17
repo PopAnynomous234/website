68db9dd65bef1555f9964baaee1323ffa4f7c5d2
PopAnynomous234 <annameeshwar@gmail.com>
2026-01-17 12:03:02 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,11 +1,9 @@
 self.__uv$config = {
-    prefix: 'uv/service/',
+    prefix: '/uv/service/', // This fixes the 'Max Scope' error from before
     bare: 'https://3sow4six3pqlu5t4qgyximuyrm0nlegb.lambda-url.us-east-2.on.aws/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    
-    // Using relative paths (no leading slash or "/s/")
-    handler: '/uv/uv.handler.js', 
+    handler: '/uv/uv.handler.js',
     bundle: '/uv/uv.bundle.js',
     config: '/uv/uv.config.js',
     sw: '/uv/uv.sw.js',
