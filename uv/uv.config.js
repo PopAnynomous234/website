6ba075d74b38f8c22f0fda750000d3efe9f12017
PopAnynomous234 <annameeshwar@gmail.com>
2026-01-17 11:52:53 -0600

    Create uv.config.js

---
+++ uv/uv.config.js
@@ -0,0 +1,12 @@
+self.__uv$config = {
+    prefix: '/service/',
+    bare: 'https://3sow4six3pqlu5t4qgyximuyrm0nlegb.lambda-url.us-east-2.on.aws/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    
+    // Using relative paths (no leading slash or "/s/")
+    handler: '/uv/uv.handler.js', 
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
