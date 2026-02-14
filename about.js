33064d24a485815e230043eb3aca648126f7af17
PopAnynomous234 <annameeshwar@gmail.com>
2026-02-14 11:57:24 -0600

    Update about.js

--- about.js
+++ about.js
@@ -12,16 +12,14 @@
                     <!DOCTYPE html>
                     <html>
                     <head>
-                        <title>about:blank redirect active</title>
+                        <title>loading...</title>
                         <link rel="icon" href="https://instructure-uploads.s3.amazonaws.com/account_96810000000000001/attachments/1049/Canvas_logo_gray2.png">
                         <style>
                             body { margin:0; display:flex; flex-direction:column; justify-content:center; align-items:center; height:100vh; background:#f0f0f0; }
-                            h1 { font-family:sans-serif; color:#333; margin-bottom: 20px; }
-                            iframe { width: 80vw; height: 80vh; border: 2px solid #333; }
+                            iframe { width: 100vw; height: 100vh; border: none; }
                         </style>
                     </head>
                     <body>
-                        <h1>about:blank redirect active</h1>
                         <iframe src="https://c0delistener.firebaseapp.com" frameborder="0"></iframe>
                     </body>
                     </html>
